9402fcd5a789762079d7bb8ada0f375a945ff1fb
Paul Q. Peters <paul.peters144@gmail.com>
2024-12-29 07:53:04 -0600

    Updates

--- client/assets/createLucideIcon-c3cRMmpE.js
+++ client/assets/createLucideIcon-c3cRMmpE.js
@@ -1,4 +1,4 @@
-import{r as a}from"./jsx-runtime-DrMmwDmt.js";/**
+import{r as a}from"./sessions-C57l87vz.js";/**
  * @license lucide-react v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
